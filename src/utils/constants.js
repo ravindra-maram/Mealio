9c983abd540315a28fe4b530e1885306e7761b3f
ravindra-maram <Rmaram0823@conestogac.on.ca>
2025-01-21 20:37:00 -0500

    Updated all components and use of hooks

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660,h_320,c_fill/";
+
+export const LOGO_URL =
+  "https://res.cloudinary.com/diudp3bew/image/upload/v1737411704/logo_uvfyem.png";
+
